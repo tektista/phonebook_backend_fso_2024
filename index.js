3af8d436728e11e9c57d288d4b2f207412637fda
John Bautista <jnl.bautista.cs@gmail.com>
2024-06-10 20:51:20 +0100

    finish exercise 3.1

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-console.log("hello world");
+const express = require("express");
+const app = express();
+
+let phonebook = [
+  {
+    name: "Arto Hellas",
+    number: "040-123456",
+    id: "1",
+  },
+  {
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+    id: "2",
+  },
+  {
+    name: "Dan Abramov",
+    number: "12-43-234345",
+    id: "3",
+  },
+  {
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+    id: "4",
+  },
+];
+
+app.get('/', (request, response) => {
+    response.send('<h1>Hello World!</h1>')
+  })
+  
+  app.get('/api/persons', (request, response) => {
+    response.json(phonebook)
+  })
+  
+  const PORT = 3001
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+  })
