199eb503409274301d073d558f7ba0c35e79262c
John Bautista <jnl.bautista.cs@gmail.com>
2024-06-11 21:16:47 +0100

    finish exercise 3.3

--- index.js
+++ index.js
@@ -5,22 +5,22 @@ let phonebook = [
   {
     name: "Arto Hellas",
     number: "040-123456",
-    id: "1",
+    id: 1,
   },
   {
     name: "Ada Lovelace",
     number: "39-44-5323523",
-    id: "2",
+    id: 2,
   },
   {
     name: "Dan Abramov",
     number: "12-43-234345",
-    id: "3",
+    id: 3,
   },
   {
     name: "Mary Poppendieck",
     number: "39-23-6423122",
-    id: "4",
+    id: 4,
   },
 ];
 
@@ -33,6 +33,17 @@ app.get("/", (request, response) => {
 
 app.get("/api/persons", (request, response) => {
   response.json(phonebook);
+});
+
+app.get("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  const person = phonebook.find((person) => person.id === id);
+
+  if (person) {
+    response.json(person);
+  } else {
+    response.status(404).end();
+  }
 });
 
 const PORT = 3001;
